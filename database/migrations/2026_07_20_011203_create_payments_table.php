7e9f78c0998894112e6d33351c738ab0e7e7d0f7
Santiago <santiagomorales.samac@gmail.com>
2026-07-19 20:55:31 -0500

    Primer commit - AlquilaFacil

--- database/migrations/2026_07_20_011203_create_payments_table.php
+++ database/migrations/2026_07_20_011203_create_payments_table.php
@@ -6,18 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('payments', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('payments');
     }
 };
